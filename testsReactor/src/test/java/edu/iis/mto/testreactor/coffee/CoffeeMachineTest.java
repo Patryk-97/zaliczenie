76259fe03fa70fe263bcc756d3a8568e5cb12ca9
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-06-08 17:41:17 +0200

    Prepare mocks

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -3,9 +3,29 @@ package edu.iis.mto.testreactor.coffee;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
 
 class CoffeeMachineTest {
+
+    @Mock
+    private Grinder grinder;
+
+    @Mock
+    private MilkProvider milkProvider;
+
+    @Mock
+    private CoffeeReceipes receipes;
+
+    private CoffeeMachine coffeeMachine;
+
+    @BeforeEach
+    void setUp() {
+        coffeeMachine = new CoffeeMachine(grinder, milkProvider, receipes);
+    }
 
     @Test
     public void itCompiles() {
