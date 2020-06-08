27c533f8158a20212b3b6ce822d5b2a8cb314426
Patryk.Stepien <215886@edu.p.lodz.pl>
2020-06-08 18:16:55 +0200

    First test case

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -1,14 +1,15 @@
 package edu.iis.mto.testreactor.coffee;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
 
+@ExtendWith(MockitoExtension.class)
 class CoffeeMachineTest {
 
     @Mock
@@ -20,16 +21,37 @@ class CoffeeMachineTest {
     @Mock
     private CoffeeReceipes receipes;
 
+    @Mock
+    private CoffeeReceipe receipe;
+
     private CoffeeMachine coffeeMachine;
+    private CoffeeSize unrelevantCoffeeSize = CoffeeSize.SMALL;
+    private CoffeType unrelevantCoffeeType = CoffeType.CAPUCCINO;
 
     @BeforeEach
     void setUp() {
         coffeeMachine = new CoffeeMachine(grinder, milkProvider, receipes);
+        unrelevantCoffeeSize = CoffeeSize.SMALL;
+        unrelevantCoffeeType = CoffeType.CAPUCCINO;
+        Mockito.when(grinder.canGrindFor(unrelevantCoffeeSize))
+               .thenReturn(true);
+        Mockito.when(receipes.getReceipe(unrelevantCoffeeType))
+               .thenReturn(receipe);
     }
 
     @Test
-    public void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
+    public void ShouldCallGrinderAndMilkProviderandCoffeeMachine() {
+        CoffeeSize unrelevantCoffeeSize = CoffeeSize.SMALL;
+        CoffeType unrelevantCoffeeType = CoffeType.CAPUCCINO;
+        CoffeOrder coffeeOrder = coffeeOrder(unrelevantCoffeeSize, unrelevantCoffeeType);
+        Coffee coffee = coffeeMachine.make(coffeeOrder);
+    }
+
+    private CoffeOrder coffeeOrder(CoffeeSize unrelevantCoffeeSize, CoffeType unrelevantCoffeeType) {
+        return CoffeOrder.builder()
+                         .withSize(unrelevantCoffeeSize)
+                         .withType(unrelevantCoffeeType)
+                         .build();
     }
 
 }
